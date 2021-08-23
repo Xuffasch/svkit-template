e63cc69c54622ad20d9eca74d51788b02c53fbdf
Xuffasch <tristan.lucas@gmail.com>
2021-08-23 11:19:31 +0200

    Setup @typescript-eslint with type-aware checks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,14 +7,26 @@ module.exports = {
     'prettier',
   ],
   plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  ignorePatterns: ['*.cjs', '*.config.js'],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+    },
+  ],
   settings: {
     'svelte3/typescript': () => require('typescript'),
   },
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2019,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
   },
   env: {
     browser: true,
